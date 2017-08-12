85bb47e7b053c4a3a9c3ef59cda50a068e650559
Ivan Li <email2liyang@gmail.com>
2017-08-12 20:05:04 +0800

    Tuples

---
+++ src/main/scala/ch04/TuplesType.scala
@@ -0,0 +1,11 @@
+package ch04
+
+object TuplesType extends App {
+
+  val t = ("Xinran", "Amy", "Li")
+  val (firstName, middleName, lastName) = t
+  println(firstName)
+  println(middleName)
+  println(lastName)
+
+}
