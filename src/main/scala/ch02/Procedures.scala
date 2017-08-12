a18c54e4a4a4bcd0f2bba56b7bfe824c898a79fb
Ivan Li <email2liyang@gmail.com>
2017-08-12 10:44:12 +0800

    add Procedures

--- src/main/scala/ch02/Procedures.scala
+++ src/main/scala/ch02/Procedures.scala
@@ -1,10 +1,22 @@
 package ch02
 
 object Procedures extends App{
+  /*
+  “The concise procedure syntax can be a surprise for Java and C++ programmers.
+  It is a common error to accidentally omit the = in a function definition.
+  You then get an error message at the point where the function is called,
+  and you are told that Unit is not acceptable at that location.”
+  Excerpt From: Horstmann, Cay S. “Scala for the Impatient.” iBooks.
+   */
+
+  def theBox(word:String)  {
+    box(word)
+  }
   def box(word:String):Unit = {
     val border = "-"*word.length + "--\n"
     println(border+"|"+word+"|\n"+border)
   }
 
   box("hello")
+  theBox("hello")
 }
