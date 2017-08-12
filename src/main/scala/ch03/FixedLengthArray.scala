b858118f407fe6830e1f22406b5203a65dacee8b
Ivan Li <email2liyang@gmail.com>
2017-08-12 13:04:44 +0800

    add FixedLengthArray

---
+++ src/main/scala/ch03/FixedLengthArray.scala
@@ -0,0 +1,9 @@
+package ch03
+
+object FixedLengthArray extends App {
+
+  val nums = new Array[Int](10)
+  val names = new Array[String](10)
+  names(0) = "a"
+  val names2 = Array("hello", "world")
+}
